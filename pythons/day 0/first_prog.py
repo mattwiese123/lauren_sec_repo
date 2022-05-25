013935329d52ce2b5e1219f0868b6073ecb404b9
Matthew Wiese <mattwiese123@gmail.com>
2022-05-24 23:18:22 -0500

    finished day 0

---
+++ pythons/day 0/first_prog.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x_int = 3 # int
+x_float = float(x_int) # float()
+
+print(x_int, x_float)
+
+y_float = 3.8 # float
+y_int = int(y_float) # int
+
+print(y_float, y_int)
+
+# z_string = "string" # string
+# z_int = int(z_string) # int?
+
+# print(z_string, z_int)
+
